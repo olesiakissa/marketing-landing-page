1e86a8f125602c973102cb02e5c253f24ef4a8b5
Olesia Kissa <okissaw@gmail.com>
2023-07-06 12:20:36 +0200

    Add basic page structure, styles and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import styles from './style'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from './components'
+
+const App = () => (
+  <main className='bg-primary w-full overflow-hidden'>
+    {/* navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    {/* hero section */}
+    <header className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </header>
+
+    {/* Features sections*/}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing /> 
+        <CardDeal />
+        <Testimonials /> 
+        <Clients /> 
+        <CTA /> 
+        <Footer /> 
+      </div>
+    </div>
+  </main>
+)
 
 export default App
